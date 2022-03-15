7228be1f707d743a65f522424b3596d6c2d8e5ef
ttomn <tomn@cs.huji.ac.il>
2022-03-15 10:46:11 +0200

    questions 2 and 3

--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -4,12 +4,13 @@ from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
 
 TRUE_EXPECTATION = 10
 
-def test_univariate_gaussian():
 
+def test_univariate_gaussian():
     # Question 1 - Draw samples and print fitted model
     gaussian_1000_samples = np.random.normal(TRUE_EXPECTATION, 1, size=1000)
     sample_1 = UnivariateGaussian()
@@ -17,16 +18,26 @@ def test_univariate_gaussian():
     print(sample_1.mu_, sample_1.var_)
 
     # Question 2 - Empirically showing sample mean is consistent
-    samples_2 = np.arange(10,1010,10)
     samples_2_distance = np.zeros(100)
-    samples_2_amount = np.arange(10,1010,10)
+    samples_2_amount = np.arange(10, 1010, 10)
     for amount in samples_2_amount:
-        sample_1.fit(gaussian_1000_samples[amount])
-        samples_2_distance[amount/10-1] = np.abs(sample_1.mu_-TRUE_EXPECTATION)
-
+        sample_1.fit(gaussian_1000_samples[:amount])
+        samples_2_distance[amount // 10 - 1] = abs(sample_1.mu_ - TRUE_EXPECTATION)
+    fig = make_subplots(rows=1, cols=1) \
+        .add_traces(
+        [go.Scatter(x=samples_2_amount, y=samples_2_distance, mode='lines', marker=dict(color="black"),
+                    showlegend=False)]) \
+        .update_layout(title_text=r"$\text{bias as a function of sample size}$", height=300) \
+        .update_xaxes(title_text="Number of samples") \
+        .update_yaxes(title_text="bias")
+    fig.show()
 
     # Question 3 - Plotting Empirical PDF of fitted model
-    raise NotImplementedError()
+    pdf_samples_3 = sample_1.pdf(gaussian_1000_samples)
+    go.Figure([go.Scatter(x=gaussian_1000_samples, y=pdf_samples_3, mode='markers')],
+              layout=go.Layout(title=r"$\text{PDF as a function of the samples values}$",
+                               xaxis_title="samples values", yaxis_title="PDF", height=300)).show()
+    return
 
 
 def test_multivariate_gaussian():
