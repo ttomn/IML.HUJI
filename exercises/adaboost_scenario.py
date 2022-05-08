5a7a9882864e6d000029ae144b28f3b50cb65b8b
ttomn <tomn@cs.huji.ac.il>
2022-05-08 13:06:44 +0300

    q1-4 finished but algorithms don't work well

--- exercises/adaboost_scenario.py
+++ exercises/adaboost_scenario.py
@@ -38,6 +38,19 @@ def generate_data(n: int, noise_ratio: float) -> Tuple[np.ndarray, np.ndarray]:
     return X, y
 
 
+def decision_surface1(predict, xrange, yrange, t, density=120, dotted=False, colorscale=custom, \
+                      showscale=True):
+    xrange, yrange = np.linspace(*xrange, density), np.linspace(*yrange, density)
+    xx, yy = np.meshgrid(xrange, yrange)
+    pred = predict(np.c_[xx.ravel(), yy.ravel()], t)
+    if dotted:
+        return go.Scatter(x=xx.ravel(), y=yy.ravel(), opacity=1, mode="markers",
+                          marker=dict(color=pred, size=1, colorscale=colorscale, reversescale=False),
+                          hoverinfo="skip", showlegend=False)
+    return go.Contour(x=xrange, y=yrange, z=pred.reshape(xx.shape), colorscale=colorscale, reversescale=False,
+                      opacity=.7, connectgaps=True, hoverinfo="skip", showlegend=False, showscale=showscale)
+
+
 def fit_and_evaluate_adaboost(noise, n_learners=250, train_size=5000, test_size=500):
     (train_X, train_y), (test_X, test_y) = generate_data(train_size, noise), generate_data(test_size, noise)
 
@@ -59,18 +72,62 @@ def fit_and_evaluate_adaboost(noise, n_learners=250, train_size=5000, test_size=
 
     fig.show()
 
-    # # Question 2: Plotting decision surfaces
-    # T = [5, 50, 100, 250]
-    # lims = np.array([np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array(
-    #     [-.1, .1])
-    # raise NotImplementedError()
-    #
-    # # Question 3: Decision surface of best performing ensemble
-    # raise NotImplementedError()
-    #
-    # # Question 4: Decision surface with weighted samples
-    # raise NotImplementedError()
+    # Question 2: Plotting decision surfaces
+    T = [5, 50, 100, 250]
+    symbols = np.array(["circle", "x"])
+    lims = np.array([np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array(
+        [-.1, .1])
+    fig = make_subplots(rows=2, cols=2, subplot_titles=[rf"$\textbf{{{m} iterations}}$" for m in T],
+                        horizontal_spacing=0.01, vertical_spacing=.03)
+    for i, t in enumerate(T):
+        fig.add_traces([decision_surface1(learner.partial_predict, lims[0], lims[1], t=t, showscale=False),
+                        go.Scatter(x=train_X[:, 0], y=train_X[:, 1], mode="markers", showlegend=False,
+                                   marker=dict(color=train_y, symbol=symbols[0],
+                                               colorscale=[
+                                                   custom[0],
+                                                   custom[-1]],
+                                               line=dict(color="black", width=1))),
+                        go.Scatter(x=test_X[:, 0], y=test_X[:, 1], mode="markers", showlegend=False,
+                                   marker=dict(color=test_y, symbol=symbols[1],
+                                               colorscale=[
+                                                   custom[0],
+                                                   custom[-1]],
+                                               line=dict(color="black", width=1)))],
+                       rows=(i // 2) + 1, cols=(i % 2) + 1)
 
+    fig.update_layout(title=rf"$\textbf{{(2) Decision Boundaries Using AdaBoost}}$",
+                      margin=dict(t=100)) \
+        .update_xaxes(visible=False).update_yaxes(visible=False)
+    fig.show()
+
+    # Question 3: Decision surface of best performing ensemble
+    best_t = losses_test.argmin()
+    go.Figure(
+        data=[decision_surface1(learner.partial_predict, lims[0], lims[1], t=best_t + 1, showscale=False),
+              go.Scatter(x=test_X[:, 0], y=test_X[:, 1], mode="markers", showlegend=False,
+                         marker=dict(color=test_y, symbol=symbols[1],
+                                     colorscale=[
+                                         custom[0],
+                                         custom[-1]],
+                                     line=dict(color="black", width=1)))],
+        layout=go.Layout(title=rf"$\textbf{{Best Ensemble Size is {best_t + 1} With Loss "
+                               rf"{losses_test[best_t]}}}$")).update_xaxes(
+        visible=False).update_yaxes(
+        visible=False).show()
+
+    # Question 4: Decision surface with weighted samples
+    go.Figure(
+        data=[decision_surface1(learner.partial_predict, lims[0], lims[1], t=n_learners, showscale=False),
+              go.Scatter(x=train_X[:, 0], y=train_X[:, 1], mode="markers", showlegend=False,
+                         marker=dict(size=learner.D_/np.max(learner.D_) * 5, color=train_y, symbol=symbols[0],
+                                     colorscale=[
+                                         custom[0],
+                                         custom[-1]],
+                                     line=dict(color="black", width=1)))],
+        layout=go.Layout(title=rf"$\textbf{{heyyyyyyyyyyyyyyyyyyyyyyyy {best_t + 1} Loss "
+                               rf"{losses_test[best_t]}}}$")).update_xaxes(
+        visible=False).update_yaxes(
+        visible=False).show()
 
 if __name__ == '__main__':
     np.random.seed(0)
